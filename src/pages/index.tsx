906983322b57204d9a74478ad62e05500f1e1afb
darkeaee <jvcq13@gmail.com>
2022-02-12 15:43:22 -0300

    changed home page to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "../styles/pagesStyles/home.module.scss";
 import { stripe } from "../services/stripe";
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KRj0SKoitKUQVCgwJ9REKun");
 
   const product = {
@@ -51,5 +55,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
